4740ffce6c8144310d4e75d8be8875788ceeb16b
akhsanby <akhsanby@gmail.com>
2024-02-10 07:25:51 +0700

    initial template for login & register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="h-screen">
       <div className="flex h-full items-center flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="overflow-hidden rounded-lg bg-white shadow sm:w-[350px] lg:w-[400px]">
           <div className="px-4 py-5 sm:p-6">
             <div className="sm:mx-auto sm:w-full ">
-              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login to your account</h2>
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register new account</h2>
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full">
               <form className="space-y-3" action="#" method="POST">
+                <div>
+                  <label htmlFor="fullName" className="block text-sm font-medium leading-6 text-gray-900">
+                    Full Name
+                  </label>
+                  <div className="mt-1">
+                    <input id="fullName" name="fullName" type="text" required className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+                </div>
                 <div>
                   <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                     Email address
@@ -31,18 +39,28 @@ export default function Login() {
                     <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   </div>
                 </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Confirm Password
+                    </label>
+                  </div>
+                  <div className="mt-1">
+                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+                </div>
 
                 <div>
                   <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                    Login
+                    Register
                   </button>
                 </div>
               </form>
 
               <p className="mt-5 text-center text-sm text-gray-500">
-                Not registered?{" "}
-                <Link href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                  Register here
+                Already registered?{" "}
+                <Link href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Login here
                 </Link>
               </p>
             </div>
