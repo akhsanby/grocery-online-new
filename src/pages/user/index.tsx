9f8e5db07bff738ecd5f8573718c1938bc29f2c3
akhsanby <akhsanby@gmail.com>
2024-02-12 15:20:56 +0700

    initial user page

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,115 @@
+import Layout from "@/components/Layout";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+
+export default function User() {
+  return (
+    <Layout>
+      <form className="p-10">
+        <div className="space-y-12">
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly so be careful what you share.</p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                  About
+                </label>
+                <div className="mt-2">
+                  <textarea id="about" name="about" rows={3} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                </div>
+                <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                  Photo
+                </label>
+                <div className="mt-2 flex items-center gap-x-3">
+                  <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                  <button type="button" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                    Change
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="full-name" className="block text-sm font-medium leading-6 text-gray-900">
+                  Full name
+                </label>
+                <div className="mt-2">
+                  <input id="full-name" name="full-name" type="full-name" autoComplete="full-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input id="email" name="email" type="email" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+                  Address
+                </label>
+                <div className="mt-2">
+                  <input type="text" name="street-address" id="street-address" autoComplete="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Security</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">Use to change your current password</p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="full-name" className="block text-sm font-medium leading-6 text-gray-900">
+                  Current password
+                </label>
+                <div className="mt-2">
+                  <input id="full-name" name="full-name" type="full-name" autoComplete="full-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  New password
+                </label>
+                <div className="mt-2">
+                  <input id="email" name="email" type="email" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+                  Confirm password
+                </label>
+                <div className="mt-2">
+                  <input type="text" name="street-address" id="street-address" autoComplete="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+            Cancel
+          </button>
+          <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Save
+          </button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
