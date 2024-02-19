641fec0c2dfc640265e766bb14cf88d85b684217
akhsanby <akhsanby@gmail.com>
2024-02-19 11:54:15 +0700

    user service with registr & login

---
+++ backend/services/user-service.js
@@ -0,0 +1,71 @@
+import prismaClient from "../app/database.js";
+import { ResponseError } from "../error/response-error.js";
+import { registerValidation, loginValidation } from "../validation/user-validation.js";
+import validate from "../validation/validate.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+
+async function register(request) {
+  const registerRequest = validate(registerValidation, request);
+
+  const isCustomerExist = await prismaClient.user.findFirst({
+    where: {
+      email: registerRequest.email,
+    },
+  });
+
+  if (isCustomerExist) throw new ResponseError(400, "Email already registered, try other");
+
+  // do password hash with bcrypt
+  registerRequest.password = await bcrypt.hash(registerRequest.password, 10);
+
+  const newCustomer = await prismaClient.user.create({
+    data: {
+      full_name: registerRequest.full_name,
+      email: registerRequest.email,
+      password: registerRequest.password,
+    },
+    select: {
+      full_name: true,
+      email: true,
+      role: true,
+    },
+  });
+
+  return newCustomer;
+}
+
+async function login(request) {
+  const loginRequest = validate(loginValidation, request);
+
+  const customer = await prismaClient.user.findFirst({
+    where: {
+      email: loginRequest.email,
+    },
+  });
+
+  // if user not found
+  if (!customer) throw new ResponseError(401, "Email or password wrong");
+
+  // do password compare with bcrypt
+  const isPasswordValid = await bcrypt.compare(loginRequest.password, customer.password);
+
+  if (!isPasswordValid) throw new ResponseError(401, "Email or password wrong");
+
+  return await prismaClient.user.update({
+    data: {
+      token: uuid(),
+    },
+    where: {
+      email: customer.email,
+    },
+    select: {
+      token: true,
+    },
+  });
+}
+
+export default {
+  register,
+  login,
+};
