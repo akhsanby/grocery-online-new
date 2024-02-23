252b8333ca58d0b633f37eacc0520625fa54db05
akhsanby <akhsanby@gmail.com>
2024-02-23 10:20:32 +0700

    new route api: get user

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth-middleware.js";
+import userController from "../controllers/user-controller.js";
 
 const userRouter = new express.Router();
 userRouter.use(authMiddleware);
+
+userRouter.get("/api/user", userController.get);
 
 export { userRouter };
