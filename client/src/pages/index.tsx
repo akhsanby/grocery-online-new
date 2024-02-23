d503a6ab88279d53f4fdc17f521d960dcee66be7
akhsanby <akhsanby@gmail.com>
2024-02-23 09:29:42 +0700

    integrate login function

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,28 @@
+import axiosClient from "@/utils/axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useLoginStore } from "@/utils/store/auth";
+import { AlertSuccess } from "@/components/Alert";
+import Cookies from "js-cookie";
 
 export default function Login() {
   const router = useRouter();
+  const { email, password, setEmail, setPassword, reset, successRegister } = useLoginStore((state) => state);
+
+  function handleLogin(e: any) {
+    e.preventDefault();
+    axiosClient
+      .post("/api/user/login", { email, password })
+      .then((result) => {
+        const { token } = result.data.data;
+        Cookies.set("token", token);
+        router.replace("/home");
+      })
+      .catch((err) => {
+        reset();
+        console.error(err);
+      });
+  }
 
   return (
     <div className="h-screen">
@@ -12,15 +32,15 @@ export default function Login() {
             <div className="sm:mx-auto sm:w-full ">
               <h2 className="my-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login to your account</h2>
             </div>
-
+            {successRegister && <AlertSuccess />}
             <div className="mt-10 sm:mx-auto sm:w-full">
-              <form className="space-y-3" onSubmit={() => router.push("/home")}>
+              <form className="space-y-3" onSubmit={(e) => handleLogin(e)}>
                 <div>
                   <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                     Email address
                   </label>
                   <div className="mt-1">
-                    <input id="email" type="email" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   </div>
                 </div>
 
@@ -31,12 +51,12 @@ export default function Login() {
                     </label>
                   </div>
                   <div className="mt-1">
-                    <input id="password" type="password" className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                   </div>
                 </div>
 
                 <div>
-                  <button type="button" onClick={() => router.replace("/home")} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Login
                   </button>
                 </div>
